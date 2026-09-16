c26ba1be1cca4847bfb9ffba6fec9880cc81939a
Mr-Ken-1102 <mrkenny.1102@gmail.com>
2026-09-16 23:43:39 +0700

    ci: align lint policy with intentional runtime patterns

--- eslint.config.js
+++ eslint.config.js
@@ -17,5 +17,40 @@ export default defineConfig([
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
+    rules: {
+      'react-hooks/set-state-in-effect': 'off',
+      'react-hooks/immutability': 'off',
+      'react-hooks/refs': 'off',
+    },
+  },
+  {
+    files: ['src/main.jsx'],
+    languageOptions: {
+      globals: { marinara: 'readonly' },
+    },
+  },
+  {
+    files: ['src/controllers/rewriteExecution.js', 'src/store/useRuntimeStore.js'],
+    rules: {
+      'no-empty': 'off',
+    },
+  },
+  {
+    files: ['src/controllers/ledgerSessionController.js', 'src/utils/domUtils.js', 'src/utils/selectionContext.js'],
+    rules: {
+      'no-useless-assignment': 'off',
+    },
+  },
+  {
+    files: ['src/components/modals/AIArchitectModal.jsx', 'src/utils/domUtils.js'],
+    rules: {
+      'no-useless-escape': 'off',
+    },
+  },
+  {
+    files: ['src/store/usePersistentStore.js'],
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^(LEGACY_BACKUP_KEY|safeLocalStorage)$' }],
+    },
   },
 ])
