ed3f7b115b75e5773a7d0213dd5d085e13833c79
Mr-Ken-1102 <mrkenny.1102@gmail.com>
2026-09-17 13:59:01 +0700

    wire popup accessibility calibration layer

--- src/styles-popup.js
+++ src/styles-popup.js
@@ -1,5 +1,6 @@
 import { RWA_POPUP_BASE_CSS } from './styles-popup-base.js';
 import { RWA_POPUP_CONTEXT_CSS } from './styles-popup-context.js';
+import { RWA_POPUP_A11Y_CSS } from './styles-popup-a11y.js';
 import { RWA_POPUP_RESPONSIVE_CSS } from './styles-popup-responsive.js';
 
-export const RWA_POPUP_CSS = `${RWA_POPUP_BASE_CSS}\n${RWA_POPUP_CONTEXT_CSS}\n${RWA_POPUP_RESPONSIVE_CSS}`;
+export const RWA_POPUP_CSS = `${RWA_POPUP_BASE_CSS}\n${RWA_POPUP_CONTEXT_CSS}\n${RWA_POPUP_A11Y_CSS}\n${RWA_POPUP_RESPONSIVE_CSS}`;
