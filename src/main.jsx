ce4ca1190daf8c41992b6e3f7b4ca82bb5b6e221
Mr-Ken-1102 <mrkenny.1102@gmail.com>
2026-09-17 21:45:51 +0700

    ui: load isolated popup settings and result layers

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { RWA_WORLDCLASS_CSS } from './styles-worldclass.js';
 import { RWA_PERFORMANCE_CSS } from './styles-performance.js';
 import { RWA_SETTINGS_CSS } from './styles-settings.js';
 import { RWA_POPUP_CSS } from './styles-popup.js';
+import { RWA_RESULT_CSS } from './styles-result.js';
 import { diffWorkerInstance } from './services/diffWorkerService';
 import { usePersistentStore } from './store/usePersistentStore';
 import { MarinaraHost } from './services/marinaraHost';
@@ -96,7 +97,7 @@ import { sessionLedgerStore } from './services/advancedRewriteService';
 
     const styleContainer = document.createElement('style');
     styleContainer.id = 'rwa-premium-styles';
-    styleContainer.textContent = `${RWA_PREMIUM_CSS}\n${RWA_BALANCE_CSS}\n${RWA_WORLDCLASS_CSS}\n${RWA_PERFORMANCE_CSS}\n${RWA_SETTINGS_CSS}\n${RWA_POPUP_CSS}`;
+    styleContainer.textContent = `${RWA_PREMIUM_CSS}\n${RWA_BALANCE_CSS}\n${RWA_WORLDCLASS_CSS}\n${RWA_PERFORMANCE_CSS}\n${RWA_POPUP_CSS}\n${RWA_SETTINGS_CSS}\n${RWA_RESULT_CSS}`;
     shadowRoot.appendChild(styleContainer);
 
     if (destroyed) {
