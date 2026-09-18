32fd86dfc693e54caa43879c4948271064be55b1
Mr-Ken-1102 <mrkenny.1102@gmail.com>
2026-09-18 11:31:13 +0700

    fix: include rewrite speed controls in ledger execution signature

--- src/services/advancedRewriteService.js
+++ src/services/advancedRewriteService.js
@@ -6,8 +6,8 @@ const MAX_LEDGER_SLICE_CHARS = 24_000;
 
 const LEDGER_INFERENCE_CONFIG_KEYS = Object.freeze([
   'connMode', 'connectionId', 'ollamaUrl', 'ollamaModel', 'extenderUrl',
-  'directTemp', 'requestTimeoutMs', 'maxPromptChars', 'lengthEnabled', 'lengthPct',
-  'conciseSysPrompt', 'freeMode', 'contextDepth', 'injectChar', 'injectUser',
+  'directTemp', 'requestTimeoutMs', 'marinaraTimeoutMs', 'maxPromptChars', 'lengthEnabled', 'lengthPct',
+  'conciseSysPrompt', 'fastRewrite', 'freeMode', 'contextDepth', 'injectChar', 'injectUser',
   'injectLorebook', 'localContextEnabled', 'localContextWords', 'useExtenderMemory',
   'speakerAware', 'charCardIds',
 ]);
