eb222f1db2229d8b7d49016007c4b1a907bc442f
Mr-Ken-1102 <mrkenny.1102@gmail.com>
2026-09-20 12:11:09 +0700

    set compact popup and undo defaults

--- src/store/persistence/schema.js
+++ src/store/persistence/schema.js
@@ -34,8 +34,8 @@ export const DEFAULT_PROFILES = [
 
 export const DEFAULT_CONFIG = {
   uiLanguage: 'en',
-  cols: 3,
-  rows: 5,
+  cols: 4,
+  rows: 4,
   typewriter: true,
   showDiff: true,
   lengthEnabled: false,
@@ -43,7 +43,7 @@ export const DEFAULT_CONFIG = {
   autoApply: false,
   popupPos: 'auto',
   pinnedPos: null,
-  historyDepth: 1,
+  historyDepth: 5,
   compact: false,
   conciseSysPrompt: false,
   fastRewrite: true,
